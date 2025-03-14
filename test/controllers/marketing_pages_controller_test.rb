46ae8db9cf3efd85324793eb7b55d97b29f86d97
Devan Beitel <devan@devanb.us>
2025-03-14 15:35:11 -0400

    test(marketing): clean up marketing controller tests

--- test/controllers/marketing_pages_controller_test.rb
+++ test/controllers/marketing_pages_controller_test.rb
@@ -1,48 +1,8 @@
 require "test_helper"
 
 class MarketingPagesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @marketing_page = marketing_pages(:one)
-  end
-
   test "should get index" do
-    get marketing_pages_url
+    get root_url
     assert_response :success
-  end
-
-  test "should get new" do
-    get new_marketing_page_url
-    assert_response :success
-  end
-
-  test "should create marketing_page" do
-    assert_difference("MarketingPage.count") do
-      post marketing_pages_url, params: { marketing_page: {} }
-    end
-
-    assert_redirected_to marketing_page_url(MarketingPage.last)
-  end
-
-  test "should show marketing_page" do
-    get marketing_page_url(@marketing_page)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_marketing_page_url(@marketing_page)
-    assert_response :success
-  end
-
-  test "should update marketing_page" do
-    patch marketing_page_url(@marketing_page), params: { marketing_page: {} }
-    assert_redirected_to marketing_page_url(@marketing_page)
-  end
-
-  test "should destroy marketing_page" do
-    assert_difference("MarketingPage.count", -1) do
-      delete marketing_page_url(@marketing_page)
-    end
-
-    assert_redirected_to marketing_pages_url
   end
 end
