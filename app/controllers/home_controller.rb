58998828f90fe5d726c0d372dec54f6868644b9b
Devan Beitel <devan@devanb.us>
2025-03-14 15:33:31 -0400

    chore(app): remove unneeded home controller

--- app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < ApplicationController
-  allow_unauthenticated_access only: %i[index]
-  before_action :resume_session, only: %i[index]
-
-  # GET /
-  def index
-    render inertia: "Home/Index"
-  end
-end
