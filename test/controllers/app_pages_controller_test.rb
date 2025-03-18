acdd2c21fd3e1395ad3f2e0c7e935af5c664883b
Devan Beitel <devan@devanb.us>
2025-03-18 10:50:21 -0400

    test(dashboard): correct testing issues

--- test/controllers/app_pages_controller_test.rb
+++ test/controllers/app_pages_controller_test.rb
@@ -1,15 +1,18 @@
 require "test_helper"
 
 class AppPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get dashboard" do
-    get dashboard_url
-    assert_response :success
-  end
-
   test "should redirect to login when not authenticated" do
     delete session_path
 
     get dashboard_url
     assert_redirected_to new_session_path
+  end
+
+  test "should get dashboard when authenticated" do
+    user = users(:one)
+    post session_path, params: { email_address: user.email_address, password: "password" }
+
+    get dashboard_url
+    assert_response :success
   end
 end
--- test/system/app_pages_test.rb
+++ test/system/app_pages_test.rb
@@ -3,12 +3,16 @@ require "application_system_test_case"
 class AppPagesTest < ApplicationSystemTestCase
   test "redirects to login when not authenticated" do
     visit dashboard_url
-    assert_current_path new_user_session_path
+    assert_current_path new_session_path
   end
 
-  test "visiting the dashboard" do
-    sign_in users(:one)
-    visit dashboard_url
+  test "navigates to dashboard after authenticating" do
+    user = users(:one)
+
+    visit new_session_path
+    fill_in "Email address", with: user.email_address
+    fill_in "Password", with: "password"
+    click_button "Log in"
     assert_selector "h1", text: "Dashboard"
   end
 end
