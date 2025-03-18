f294740f03fe224b85a203af79e11905e30c43ee
Devan Beitel <devan@devanb.us>
2025-03-18 17:27:08 -0400

    feat(register): check for pwned passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
   has_many :dreams, dependent: :destroy
+  validates :password, not_pwned: { threshold: 1, message: "has appeared in a data breach and cannot be used for security reasons. Please choose a different password.", on_error: :accept }
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
