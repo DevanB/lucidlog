4b021fe42d571838f3a068007e832bc641bfe046
Devan Beitel <devan@devanb.us>
2025-03-19 11:20:44 -0400

    feat(shared data): add auth.user to shared props

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Lucidlog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.application_name = "LucidLog"
   end
 end
