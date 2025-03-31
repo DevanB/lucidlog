e4bb187672699a8a944b2213594e0b104d6c9a85
Devan Beitel <devan@devanb.us>
2025-03-30 22:18:41 -0400

    fix(password reset): correct mailer function name

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -10,7 +10,7 @@ class PasswordsController < ApplicationController
 
   def create
     if @user = User.find_by(create_params)
-      PasswordsMailer.reset_password(@user).deliver_later
+      PasswordsMailer.reset(@user).deliver_later
     end
 
     redirect_to new_session_path, notice: "Password reset instructions sent (if user with that email address exists)."
