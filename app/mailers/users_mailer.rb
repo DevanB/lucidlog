50975bad0086880b2673b7d8cb5a6b8b02530f11
Devan Beitel <devan@devanb.us>
2025-03-30 22:16:26 -0400

    chore(emails): clean up methods

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -1,6 +1,6 @@
 class UsersMailer < ApplicationMailer
   def email_verification(user)
     @user = user
-    mail to: user.email_address, subject: "Please verify your email address"
+    mail to: @user.email_address, subject: "Please verify your email address"
   end
 end
